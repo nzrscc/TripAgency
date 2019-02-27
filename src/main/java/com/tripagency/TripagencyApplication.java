dac18fc0d29c51b6fd071fc964393dd06755fc4f
nzrscc <nzrscc@gmail.com>
2019-02-27 15:56:58 +0100

    Primo caricamento

---
+++ src/main/java/com/tripagency/TripagencyApplication.java
@@ -0,0 +1,13 @@
+package com.tripagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripagencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripagencyApplication.class, args);
+    }
+
+}
